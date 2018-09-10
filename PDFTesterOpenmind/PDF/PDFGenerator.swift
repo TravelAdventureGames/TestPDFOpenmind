2b6b2c0936ab97bfb8ba749f4a7185e666b5adca
Martijn van Gogh <martijn@traveladventuregames.nl>
2018-09-10 13:48:42 +0200

    Added saving, generating and attaching pdf to mail Added mailcomposer

--- PDFTesterOpenmind/PDF/PDFGenerator.swift
+++ PDFTesterOpenmind/PDF/PDFGenerator.swift
@@ -23,6 +23,7 @@ class PDFGenerator {
         return paths[0]
     }
 
+    // MARK: Generates and saves pdf locally
     func generatePDF(diaries: [DiaryEntry]) {
 
         let A4paperSize = CGSize(width: 595, height: 842)
@@ -30,27 +31,18 @@ class PDFGenerator {
         pdf.setContentAlignment(.left)
         pdf.addText("De Nations League. Het is een term die je de afgelopen maanden misschien wel vaker voorbij hebt zien komen. 'Iets met dat onbegrijpelijke toernooi wat geen EK is maar wel belangrijk', horen we je denken. Ingewikkeld is de opzet van de Nations League wel een beetje, maar na het lezen van dit artikel hopelijk niet meer!")
 
-        let fileName = getDocumentsDirectory().appendingPathComponent("diaryPDF.pdf")
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let fileUrl = URL(fileURLWithPath: documentsPath)
+        let endUrl = fileUrl.appendingPathComponent("diaryPDF")
+
         let pdfData = pdf.generatePDFdata()
         do {
-            try pdfData.write(to: fileName, options: .atomicWrite)
+            try pdfData.write(to: endUrl, options: .atomicWrite)
         } catch {
             print(error)
         }
 
-//        let documentDirectories = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true) {
-//
-//            let pdfData = pdf.generatePDFdata()
-//            do {
-//                let fileName = "diaryPDF.pdf"
-//                let documentsFileName = URL(fileURLWithPath: documentDirectories + "/" + fileName)
-//                try pdfData.write(to: documentsFileName, options: .atomicWrite)
-//                print("\nThe generated pdf can be found at:")
-//                print("\n\t\(documentsFileName)\n")
-//            } catch {
-//                print(error)
-//            }
-//        }
+
     }
 }
 
--- PDFTesterOpenmind/PDF/PDFMailComposer.swift
+++ PDFTesterOpenmind/PDF/PDFMailComposer.swift
@@ -21,25 +21,26 @@ class PDFMailComposer: UIViewController, MFMailComposeViewControllerDelegate {
         sendMailWithPDFAttachment()
     }
 
+    // MARK: Fetches pdf from local storage and sends it as attachments via email
     public func sendMailWithPDFAttachment() {
         if (MFMailComposeViewController.canSendMail()) {
 
-            if let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] {
-                print(directory)
-                let data = NSData(contentsOf: directory)
-                let composer = MFMailComposeViewController()
-                composer.mailComposeDelegate = self
-                composer.setSubject("Mijn dagboeken")
-                composer.setToRecipients(["martijn@traveladventuregames.nl"])
-                composer.addAttachmentData(data! as Data, mimeType: "application/pdf", fileName: "diaryPDF.pdf")
-                self.present(composer, animated: true, completion: nil)
-                return
-            }
+            let directory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let fileUrl = URL(fileURLWithPath: directory)
+            let endUrl = fileUrl.appendingPathComponent("diaryPDF")
+            let data = NSData(contentsOf: endUrl)
+
+            let composer = MFMailComposeViewController()
+            composer.mailComposeDelegate = self
+            composer.setSubject("Mijn dagboeken")
+            composer.setToRecipients(["martijn@traveladventuregames.nl"])
+            composer.addAttachmentData(data! as Data, mimeType: "application/pdf", fileName: "diaryPDF")
+            self.present(composer, animated: true, completion: nil)
+            return
         }
     }
     public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?){
         self.dismiss(animated: true, completion: nil)
-        print("send!")
     }
 
 }
